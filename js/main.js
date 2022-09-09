b2b3e3c1208bee8fe332cfdc5575e3f3b9392253
Mustafoyev <otabekshark@gmail.com>
2022-09-09 21:18:21 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// Masala 1
+
+// function addition(a, b) {
+// 	return a + b;
+// }
+
+// Masala 2
+
+// function convert(minutes) {
+// 	return minutes * 60
+// }
+
+// Masala 3
+
+// function addition(num) {
+// 	return ++num;
+// }
+
+// Masala 4
+
+// function triArea(base, height) {
+// 	return (base * height) / 2;
+// }
+
+// Masala 5
+
+// function calcAge(age) {
+// 	return age * 365;
+// }
+
+// Masala 6
+
+// function remainder(x, y) {
+// 	return x % y;
+// }
+
+// Masala 7
+
+// function footballPoints(wins, draws, losses) {
+// 	return ((wins * 3) + draws * 1);
+// }
+
+// Masala 8
+
+// function points(twoPointers, threePointers) {
+// 	return (twoPointers * 2) + (threePointers * 3);
+// }
+
+// Masala 9
+
+// function lessThan100(a, b) {
+// 	if (a + b < 100) {
+// 		return true;
+// 	}else{
+// 		return false;
+// 	}
+// }
+
+// Masala 10
+
+// function animals(chickens, cows, pigs) {
+// 	return (chickens * 2) + (cows * 4) + (pigs * 4);
+// }
